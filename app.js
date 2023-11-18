ba9d139b2e375f19bc79f3a18b3e45f08020adbf
MrGalletah <mdcabrera@hotmail.es>
2023-11-18 18:47:49 -0300

    mas bonito

--- app.js
+++ app.js
@@ -4,22 +4,23 @@ const userName = document.getElementById("name");
 const lastname = document.getElementById("lastname");
 const date = document.getElementById("date");
 
-console.log(userName.value, lastname.value, date.value)
-
-
 const postFetch = async () => {
   try {
-    myObj = { name: userName.value, lastname: lastname.value, date: date.value };
+    myObj = {
+      name: userName.value,
+      lastname: lastname.value,
+      date: date.value,
+    };
     const request = await fetch(URL, {
-        method: "POST",
-        body: JSON.stringify(myObj),
-        headers: {
-            "Content-Type": "application/json",
-          },
-        })
-    const response = await request.json()
-        console.log(response)
+      method: "POST",
+      body: JSON.stringify(myObj),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const response = await request.json();
+    console.log(response);
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 };
